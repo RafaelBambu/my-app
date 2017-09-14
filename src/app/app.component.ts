c5c68947787ba66e4530963dbc32a3be7ed4e59f
Luis Rafael Rivera <30272643+RafaelBambu@users.noreply.github.com>
2017-09-14 15:50:55 -0500

    Rafa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  onEdit(){
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
+  }
 }
